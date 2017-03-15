548c8d9863e8351943a63a2f294a875c05e289ce
joseph <jawnkuin@gmail.com>
2017-03-15 11:38:29 +0800

    relay stater

--- client/actions/Roles.js
+++
@@ -1,5 +0,0 @@
-import {fetchUsers} from './Users';
-
-export const CLICK_ROLE = 'CLICK_ROLE';
-
-export const clickRole = rid => dispatch => dispatch(fetchUsers(rid));
--- client/index.js
+++ client/index.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import {render} from 'react-dom';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import injectTapEventPlugin from 'react-tap-event-plugin';
 import Root from './containers/Root';
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
 
 const App = () => (
-  <MuiThemeProvider>
-    <Root />
-  </MuiThemeProvider>
+  <Root />
 );
 render((<App />), document.getElementById('root'));
